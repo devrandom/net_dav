7c94a882ef30c229043f5040ef1a09773908d2b7
Thomas Flemming <thomas.flemming@usit.uio.no>
2009-10-29 15:43:28 +0100

    added a simple spec

--- spec/net_dav_spec.rb
+++ spec/net_dav_spec.rb
@@ -1,7 +1,10 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "NetDav" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+
+  it "should be able to do propfind in a folder" do
+    dav = Net::DAV.new("http://localhost:10080/")
+    props = dav.propfind("/")
   end
+
 end
