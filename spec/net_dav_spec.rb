fa68c1f2c3a208a0d66dab9db4520db68d3a1209
devrandom <c1.github@niftybox.net>
2009-10-29 10:26:56 -0700

    reverted specs - these should be integration tests - e.g. using cucumber

--- spec/net_dav_spec.rb
+++ spec/net_dav_spec.rb
@@ -1,10 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "NetDav" do
-
-  it "should be able to do propfind in a folder" do
-    dav = Net::DAV.new("http://localhost:10080/")
-    props = dav.propfind("/")
+  it "fails" do
+    fail "hey buddy, you should probably rename this file and start specing for real"
   end
-
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-require 'net_dav'
+require 'net/dav'
 require 'spec'
 require 'spec/autorun'
 
