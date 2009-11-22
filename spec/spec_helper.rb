b645f1669115cf7f605a784f547516332c063397
Thomas Flemming <thomas.flemming@usit.uio.no>
2009-11-22 14:32:08 +0100

    remove unecessary LOAD_PATH additions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,5 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-$LOAD_PATH << File.expand_path( File.dirname(__FILE__) + '/../lib' )
 require 'rubygems'
 require 'net/dav'
 require 'spec'
