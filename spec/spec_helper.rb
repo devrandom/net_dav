6db7cd8935ed4a067ca5ea86440c89b757d631d8
Thomas Flemming <thomas.flemming@usit.uio.no>
2009-11-22 13:54:55 +0100

    added delete method

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH << File.expand_path( File.dirname(__FILE__) + '/../lib' )
 require 'rubygems'
 require 'net/dav'
 require 'spec'
@@ -8,4 +9,13 @@ require 'webdav_server'
 
 Spec::Runner.configure do |config|
 
+end
+
+# Profind helper. Returns properties or error
+def find_props_or_error(dav, path)
+  begin
+    return dav.propfind(path).to_s
+  rescue Net::HTTPServerException => e
+    return e.to_s
+  end
 end
