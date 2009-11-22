2a5e6759fb00c8a0c30bf2c61ae9b2d354bc6a1f
Thomas Flemming <thomas.flemming@usit.uio.no>
2009-11-22 13:02:14 +0100

    Added specs for propfind that uses internal webdav server

--- spec/net_dav_spec.rb
+++ spec/net_dav_spec.rb
@@ -1,7 +1,35 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Net::Dav" do
+
+  serverpid = nil
+
+  before(:all) do
+    # This is run once and only once, before all of the examples
+    # and before any before(:each) blocks.
+
+    # Start webdav server in subprocess
+    @pid = fork do
+      webdav_server(:port => 10080,:authentication => false)
+    end
+    # Wait for webdavserver to start
+    sleep(2)
+  end
+
   it "should create a Net::Dav object" do
     Net::DAV.new("http://localhost.localdomain/").should_not be_nil
   end
+
+   it "should read properties from webdav server" do
+     dav = Net::DAV.new("http://localhost:10080/")
+     @props = dav.propfind("/").to_s
+     @props.should match(/200 OK/)
+   end
+
+  after(:all) do
+    # this is run once and only once after all of the examples
+    # and after any after(:each) blocks
+    Process.kill('SIGKILL', @pid) rescue nil
+  end
+
 end
