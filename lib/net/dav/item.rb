3c057eb63ae985bb1eefcd24f23e13e88a55d717
Thomas Flemming <thomas.flemming@usit.uio.no>
2009-11-23 23:22:48 +0100

    added propfind and proppatch methods to item class

--- lib/net/dav/item.rb
+++ lib/net/dav/item.rb
@@ -13,34 +13,44 @@ module Net
 
       # Synonym for uri
       def url
-	@uri
+        @uri
       end
 
       def initialize(dav, uri, type, size) #:nodoc:
-	@uri = uri
-	@size = size.to_i rescue nil
-	@type = type
-	@dav = dav
+        @uri = uri
+        @size = size.to_i rescue nil
+        @type = type
+        @dav = dav
       end
 
       # Get content from server if needed and return as string
       def content
-	return @content unless @content.nil?
-	@content = @dav.get(@uri.path)
+        return @content unless @content.nil?
+        @content = @dav.get(@uri.path)
       end
 
       # Put content to server
       def content=(str)
-	@dav.put_string(@uri.path, str)
-	@content = str
+        @dav.put_string(@uri.path, str)
+        @content = str
+      end
+
+      # Proppatch item
+      def proppatch(xml_snippet)
+        @dav.proppatch(@uri.path,xml_snippet)
+      end
+
+      #Properties for this item
+      def propfind
+        return @dav.propfind(@uri.path)
       end
 
       def to_s #:nodoc:
-	"#<Net::DAV::Item URL:#{@uri.to_s} type:#{@type}>"
+        "#<Net::DAV::Item URL:#{@uri.to_s} type:#{@type}>"
       end
 
       def inspect #:nodoc:
-	"#<Net::DAV::Item URL:#{@uri.to_s} type:#{@type}>"
+        "#<Net::DAV::Item URL:#{@uri.to_s} type:#{@type}>"
       end
     end
   end
